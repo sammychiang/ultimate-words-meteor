303b87ced03e3524f89b893c92a2eae7d4310376
sammyjiang <jiangzhifeng2116@gmail.com>
2015-10-20 12:52:02 +0800

    add ultimate words meteor first

---
+++ ultimate-_words.js
@@ -0,0 +1,28 @@
+Words = new Mongo.Collection("words");
+
+if (Meteor.isClient) {
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function() {
+    // code to run on server at startup
+    Meteor.methods({
+      upload_word: function(word) {
+        Words.upsert({
+          word: word
+        }, {
+          $set:{lastShowTime:Date.now()},
+          $inc: {
+            counts: 1
+          }
+        });
+        result = {
+          totalWordsNum: Words.find().count()
+        };
+        console.log(result);
+        return result;
+      }
+    })
+  });
+}
